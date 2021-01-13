cd6a6f6d6277f0308f7c5464c51873709a92736c
sarah <sarahaleesia1@gmail.com>
2021-01-13 14:24:04 -0600

    adds products using db/seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+product = Product.new(name: "Strawberry", price: 8, description:"strawberry scented 3 wick candle")
+product.save
+
+product = Product.new(name: "Isalnd Dream", price: 12, description:"floral/fresh scented candle")
+product.save
+
+product = Product.new(name: "Beeswax", price: 20, description:"Organic non-scented candle")
+product.save
