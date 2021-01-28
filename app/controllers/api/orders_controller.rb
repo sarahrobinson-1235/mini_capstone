9f25cd3fbb2a5be07e92f47039845380d1a778e7
sarah <sarahaleesia1@gmail.com>
2021-01-28 03:40:01 -0600

    adds orders actions

---
+++ app/controllers/api/orders_controller.rb
@@ -0,0 +1,34 @@
+class Api::OrdersController < ApplicationController
+  before_action :authenticate_user
+
+  def index
+    @orders = current_user.orders
+    render "index.json.jb"
+  end
+  
+  
+  def create
+    @order = Order.new(
+      product_id: params[:product_id], 
+      quantity: params[:quantity], 
+      user_id: current_user.id
+      )
+    @order.subtotal = @order.quantity * @order.product.price
+    @order.tax = @order.subtotal * 0.09
+    @order.total = @order.subtotal + @order.tax
+    if @order.save
+      render "show.json.jb"
+    else 
+      render json: {errors: @order.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @order = current_user.orders.find(params[:id])
+    if @order
+      render "show.json.jb"
+    else
+      render json: {}, status: :unauthorized
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,8 @@ Rails.application.routes.draw do
     post "/users" => "users#create"
     post "/sessions" => "sessions#create"
 
+    post "/orders" => "orders#create"
+    get "/orders" => "orders#index"
+    get "/orders/:id" => "orders#show"
   end
 end
